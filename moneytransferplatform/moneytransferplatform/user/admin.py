32b0551996ce3188c6e03cf23c27b3f85b6a563e
Nazmul Hasan <nazmul0hasan0@gmail.com>
2022-07-01 21:11:33 +0600

    MT: models created

--- moneytransferplatform/moneytransferplatform/user/admin.py
+++ moneytransferplatform/moneytransferplatform/user/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+	list_display = [
+		'id', 'profileId', 'user', 'name', 'mobileNumber', 'balance'
+	]
+
+class UserTransactionAdmin(admin.ModelAdmin):
+	list_display = [
+		'id', 'transactionId', 'userProfile', 'amount', 'isSuccessful'
+	]
--- moneytransferplatform/moneytransferplatform/user/models.py
+++ moneytransferplatform/moneytransferplatform/user/models.py
@@ -1,3 +1,23 @@
+import uuid
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+	id =  models.BigAutoField(primary_key=True)
+	profileId = \
+		models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+	user = models.OneToOneField(User, on_delete=models.PROTECT)
+	name = models.CharField(max_length=50)
+	mobileNumber = models.CharField(max_length=14)
+	balance = models.DecimalField(max_digits=12, decimal_places=2)
+
+	def __str__(self):
+		return str(self.profileId) + " - " + str(self.name)
+
+class UserTransaction(models.Model):
+	id = models.BigAutoField(primary_key=True)
+	transactionId = \
+		models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+	userProfile = models.ForeignKey(Profile, on_delete=models.PROTECT)
+	amount = models.DecimalField(max_digits=12, decimal_places=2)
+	isSuccessful = models.BooleanField(default=False)
