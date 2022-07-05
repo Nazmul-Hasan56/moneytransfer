4156dadd63c094e50a08f021b7de646f17b91b50
Nazmul Hasan <nazmul0hasan0@gmail.com>
2022-07-05 17:28:55 +0600

    MT: fix schedule user add issue

--- moneytransferplatform/moneytransferplatform/user/validation/addschedulervalidation.py
+++ moneytransferplatform/moneytransferplatform/user/validation/addschedulervalidation.py
@@ -3,12 +3,17 @@ from dataclasses import dataclass
 from library.validationresponse import ValidationResponse
 from library.error import errorutil
 from moneytransferplatform.user import userutil
-from moneytransferplatform.user.models import Profile
+from moneytransferplatform.user.models import Profile, ScheduledUser
 
 @dataclass(frozen=True)
 class AddScheduleUserParam:
 	userProfile: Profile
 	receiverProfile: Profile
+
+def isScheduleUserExist(userProfile, receiverProfile):
+	return ScheduledUser.objects.filter(
+		sender=userProfile, reciever=receiverProfile
+	).exists()
 
 def  canUserAddScheduler(user, userId, requestParam):
 	userValidation = userutil.getValidatedUserProfile(user)
@@ -21,5 +26,7 @@ def  canUserAddScheduler(user, userId, requestParam):
 		userutil.getUserProfileByProfileId(requestParam['recieverId'])
 	if not receiverValidation.isValid:
 		return receiverValidation
+	if isScheduleUserExist(userProfile, receiverValidation.response):
+		return errorutil.get400Error('Scheduled user already exist', False)
 	response = AddScheduleUserParam(userProfile, receiverValidation.response)
 	return ValidationResponse(True, response, None)
